a24e2b1492bf8a614d86d3154b0a985f34715527
BigWendl <37221842+BigWendl@users.noreply.github.com>
2018-03-11 22:06:39 -0500

    Update Infix.h

--- GitHubProjectFolder/Infix.h
+++ GitHubProjectFolder/Infix.h
@@ -1,5 +1,23 @@
-class Infix {
+namespace INF {
+	class Infix {
+#include "Stack.h"
+#include <string>
+
+		using namespace std;
+	private:
+		Stack oppo;
+		
+	public:
+
+		int evaluate(string expression)
+		{
+			
+		}
+		void oppoEvaluate()
+		{
+		
+		}
 
 
-
-};
+	};
+}
