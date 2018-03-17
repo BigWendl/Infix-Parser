5948c47459916948150638bf277e4aab4353be2a
BigWendl <37221842+BigWendl@users.noreply.github.com>
2018-03-17 15:15:56 -0500

    Update InfixProject.cpp

--- GitHubProjectFolder/InfixProject.cpp
+++ GitHubProjectFolder/InfixProject.cpp
@@ -5,19 +5,27 @@
 using namespace std;
 
 void main()
-{
+{	
 	Infix eval;
 	string expression;
 	int eval_result;
+
 	expression = "1+2*3";
 	eval_result = eval.evaluate(expression);
 	cout << "1+2*3 = " << eval_result << endl;
+
 	expression = "2+2^2*3";
 	eval_result = eval.evaluate(expression);
-	cout << "2+2^2*3 = " <<  eval_result << endl;
-	expression = "5>=10 || 2<1";
+	cout << "2+2^2*3 = " << eval_result << endl;
+
+	expression = "1==2";
 	eval_result = eval.evaluate(expression);
-	cout << "5>=10 || 2<1 = " << eval_result << endl;
+	cout << "1==2 = " << eval_result << endl;
+
+	expression = "1+3 > 2";
+	eval_result = eval.evaluate(expression);
+	cout << "1+3 > 2 = " << eval_result << endl;
+	
 	system("pause");
 	//return 0;
 }
