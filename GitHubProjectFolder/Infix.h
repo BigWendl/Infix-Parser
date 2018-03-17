d7f93f53744dbd40b64167f84094b4226e53d566
japoland <japoland@hotmail.com>
2018-03-17 00:27:45 -0500

    Update Infix.h

--- GitHubProjectFolder/Infix.h
+++ GitHubProjectFolder/Infix.h
@@ -12,30 +12,38 @@ using namespace std;
 class Infix {
 
 	private:
-		stack<char> operatorStack;
+		stack<string> operatorStack;//changed to stack of strings so it could handle two character operators
 		stack<int> operandStack;
-		stack<char> boolStack;
-		int current_precedence = 0, result, lhs, rhs;
-		char op;
-		const string operators = "*/%+-";
-		const int precedence[5] = {6, 6, 6, 5, 5};
+		stack<char> boolStack;//do we need this?
+		int result, lhs, rhs, position;
+		string operators[17] = { "!", "++", "--", "^", "*", "/", "%", "+", "-", ">", ">=", "<", "<=", "==", "!=", "&&", "||"};//only operator not included is negative (-)
+		int precedence[17] = {8, 8, 8, 7, 6, 6, 6, 5, 5, 4, 4, 4, 4, 3, 3, 2, 1};
 		//const string bools = "|&";
 		string expression;
 
-		bool isoperator(char opo)
+		int isoperator(string operators[], int precedence[], string op)
 		{
-			return operators.find(opo) != std::string::npos;
+			for (int i = 0; i < 17; i++)
+			{
+				if (operators[i] == op)//search for operator
+				{
+					return precedence[i];//if found return the precedence of the operator
+				}
+				
+			}
+			return 0; //if not found return 0
+
+			//return operators.find(opo) != std::string::npos;
 		}
 		/*bool isbool(char boolian)
 		{
 			return bools.find(boolian) != std::string::npos;
 		}*/
 
-		int evaluateOperator(char opo)
+		/*int evaluateOperator(string op)
 		{
-			int position = operators.find(opo);
 			return precedence[position];
-		}
+		}*/
 		//int evaluateBool(char boolian)
 		//{
 		//	//pop bool stack
@@ -50,88 +58,133 @@ class Infix {
 
 			istringstream tokens(expression);
 			char nextChar;
+			int current_precedence = 0, previous_precedence = 0; 
+			string op = "", temp="";
 			while (tokens >> nextChar)
 			{
-				if (isdigit(nextChar))
+				if (isdigit(nextChar))//if character is a number read the value as an integer
 				{
 					tokens.putback(nextChar);
 					int num;
 					tokens >> num;
 					operandStack.push(num);
 				}
-				else if (isoperator(nextChar))
+				else
 				{
-					if (evaluateOperator(nextChar) < current_precedence)
+					op += nextChar;//if the next character is not a number add it to the operator string
+					tokens >> nextChar;//read the next character to see if it should be included in the operator
+
+					if (isdigit(nextChar))
 					{
+						tokens.putback(nextChar);//if the next character is a number, put it back to be added to the operand stack
+					}
+					else
+					{
+						op += nextChar;//if it isn't a number add it to the operator string
+					}
+
+					current_precedence = isoperator(operators, precedence, op);//find the precedence of the operator 
+					if (current_precedence == 0)//if it can't find the precedence it is because the operator is not included in the list of operators
+					{
+						cout << "Invalid Character";//output error that the operator is invalid
+						system("pause");
+					}
+					else if (current_precedence < previous_precedence)//if the precedence of the current operator is lower than the operator previous put on the stack
+					{
+						
+						temp = op;//assign the newest operator to a temp variable and process the previous operator and previous two operands
 						op = operatorStack.top();
 						operatorStack.pop();
 						rhs = operandStack.top();
 						operandStack.pop();
 						lhs = operandStack.top();
 						operandStack.pop();
-						compute(lhs, rhs, op);
+						operandStack.push(compute(lhs, rhs, op));//push the value of the operation back to the operand stack
+						operatorStack.push(temp);//push the newest operator to the stack
+						previous_precedence = current_precedence;//set the previous precedence value to that of the operator at the top of the stack
+						op = "";//clear the operator string for further processing
 					}
 					else
 					{
-						current_precedence = evaluateOperator(nextChar);
-						operatorStack.push(nextChar);
+						previous_precedence = current_precedence;//if the precedence of the new operator is not less then push it to the stack
+						operatorStack.push(op);
+						op = "";//clear the operator string for further processing
 					}
 				}
-				}
-			//after entire string has been processed the stacks are ready to be processed
-			while (!operatorStack.empty())
+			}
+			while (!operatorStack.empty())//after entire string has been processed the stacks are ready to be processed
 			{
 				rhs = operandStack.top();
 				operandStack.pop();
 				lhs = operandStack.top();
 				operandStack.pop();
 				op = operatorStack.top();
-				compute(lhs, rhs, op);
-				
+				int answer = compute(lhs, rhs, op);
+				operandStack.push(answer);
+				operatorStack.pop();
 			}
-				return operandStack.top();
+			return operandStack.top();//once operator stack has been cleared, return the last value in the operand stack
+		}
 
-			}
-				/*else if (isbool(nextChar))
+
+			/*else if (isbool(nextChar))
+			{
+				char boolian;
+				tokens >> boolian;
+
+				if (!boolStack.empty())
 				{
-					char boolian;
-					tokens >> boolian;
-
-					if (!boolStack.empty())
+					boolStack.push(boolian);
+				}
+				else
+				{
+					if (boolian == boolStack.top())
 					{
-						boolStack.push(boolian);
+						evaluateBool(boolian);
+					}
+					else
+						throw("Must have consecutive boolian values");
+				}*/
+
+
+			int compute(int lhs, int rhs, string op)//I made this if\else if instead of switch because c++ does not allow you to switch on string values
+			{
+				if (op == "+")
+				{
+					return result = lhs + rhs;
+				}
+				else if (op == "*")
+				{
+					return result = lhs * rhs;
+				}
+				else if (op == "-")
+				{
+					return result = lhs - rhs;
+				}
+				else if (op == "/")
+				{
+					if (rhs == 0)
+					{
+						cout << "Division by Zero Error";
+						system("pause");
+						EXIT_FAILURE;
 					}
 					else
 					{
-						if (boolian == boolStack.top())
-						{
-							evaluateBool(boolian);
-						}
-						else
-							throw("Must have consecutive boolian values");
-					}*/
-
-				
-		void compute(int lhs, int rhs, char op) 
-		{
-			switch (op)
-			{
-			case '+':
-				result = lhs + rhs;
-				operandStack.push(result);
-				operatorStack.pop();
-				break;
-			case '*':
-				result = lhs * rhs;
-				operandStack.push(result);
-				operatorStack.pop();
-				break;
-			default:
-				break;
-			}
-			
+						return result = lhs / rhs;
+					}
+				}
+				else if (op == "%")
+				{
+					return result = lhs % rhs;
+				}
+				else if (op == "^")
+				{
+					return result = pow(lhs, rhs);
+				}
+				//more operators need to be added and the logic applied
+				//function needs to be passed 0 for the lhs if the operator only takes one operand
 		}
 	};
 
 #endif
-//};
